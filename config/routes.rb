bc2726a1d61df5a227773cb89e3390ecde74ce44
catfriend <eakari@live.com>
2016-03-01 16:44:44 -0800

    added publications model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  resources :publications, only: [:index, :show]
+  namespace :admin do
+    resources :publications
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
