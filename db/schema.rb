d475b2ac661e2df2e092e0712047552403b3c812
catfriend <eakari@live.com>
2016-03-13 16:41:11 -0700

    added subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302003358) do
+ActiveRecord::Schema.define(version: 20160313233724) do
 
   create_table "publications", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20160302003358) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string   "stripe_user_id"
+    t.boolean  "active",         default: false, null: false
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
+  add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
