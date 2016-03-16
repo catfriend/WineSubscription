749496ee1bf1c035b2011af812a73e69c9b21b78
catfriend <eakari@live.com>
2016-03-16 15:55:11 -0700

    created users, subscriptions, sbscription cancellations

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,4 +1,5 @@
 class PublicationsController < ApplicationController
+  before_filter :check_for_subscription, only: :show
   def index
     @publications = Publication.all
   end
@@ -7,5 +8,10 @@ class PublicationsController < ApplicationController
     @publication = Publication.find(params[:id])
   end
 
-
+  def check_for_subscription
+    unless current_user.subscription.active
+      flash[:alert] = "You must be a subscriber to access this content."
+      redirect_to publications_path    
+    end 
+  end
 end
